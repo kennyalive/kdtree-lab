099e80083813710527ab9b00200787e6c75d9eb6
Artem Kharytoniuk <artemalive@gmail.com>
2017-07-22 19:02:50 +0300

    Updated the way input data is specified: benchmark only single model.

--- src/main.cpp
+++ src/main.cpp
@@ -10,43 +10,26 @@
 #include <string>
 #include <vector>
 
-const std::string all_models[] = { "data/teapot.stl", "data/bunny.stl", "data/dragon.stl"};
-const std::string all_kd_trees[] = { "data/teapot.kdtree", "data/bunny.kdtree", "data/dragon.kdtree" };
+const std::string model_path = "data/teapot.stl";
+const std::string kdtree_path = "data/teapot.kdtree";
 
-// Selects subset from all_models array to work with.
-const std::array<int, 3> model_indices { 0, 1, 2 };
-//const std::array<int, 1> model_indices { 0};
+//const std::string model_path = "data/bunny.stl";
+//const std::string kdtree_path = "data/bunny.kdtree";
 
-const std::vector<std::string> models = []()
-{
-    std::vector<std::string> result;
-    for (auto i : model_indices)
-        result.push_back(all_models[i]);
-    return result;
-} ();
+//const std::string model_path = "data/dragon.stl";
+//const std::string kdtree_path = "data/dragon.kdtree";
 
-const std::vector<std::unique_ptr<TriangleMesh>> meshes = []()
-{
-    std::vector<std::unique_ptr<TriangleMesh>> result;
-    for (int i = 0; i < models.size(); i++)
-        result.push_back(LoadTriangleMesh(models[i]));
-    return result;
-} ();
+std::unique_ptr<TriangleMesh> mesh = LoadTriangleMesh(model_path);
 
 void main_kdtree() {
-    std::vector<std::unique_ptr<KdTree>> kdTrees;
-    for (size_t i = 0; i < model_indices.size(); i++) {
-        kdTrees.push_back(std::unique_ptr<KdTree>(new KdTree(all_kd_trees[model_indices[i]], *meshes[i])));
-    }
+    auto kdtree = std::unique_ptr<KdTree>(new KdTree(kdtree_path, *mesh));
 
     printf("shooting rays (kdtree)...\n");
     random_init();
-    for (size_t i = 0; i < models.size(); i++) {
-        int timeMsec = BenchmarkKdTree(*kdTrees[i]);
-        double speed = (benchmarkRaysCount / 1000000.0) / (timeMsec / 1000.0);
-        printf("raycast performance [%-6s]: %.2f MRays/sec, (rnd = %d)\n", StripExtension(GetFileName(models[i])).c_str(), speed, random_uint32());
 
-    }
+    int timeMsec = BenchmarkKdTree(*kdtree);
+    double speed = (benchmarkRaysCount / 1000000.0) / (timeMsec / 1000.0);
+    printf("raycast performance [%-6s]: %.2f MRays/sec, (rnd = %d)\n", StripExtension(GetFileName(model_path)).c_str(), speed, random_uint32());
 
   /*  const int raysCount[3] = {32768, 64, 32};
     for (int i = 0; i < models.size(); i++) {
@@ -59,41 +42,35 @@ void main_embree() {
 
     struct Vertex { float x, y, z, a; };
     struct Triangle { int32_t v0, v1, v2; };
-    RTCScene scenes[3];
 
-    for (int i = 0; i < models.size(); i++) {
-        scenes[i] = rtcDeviceNewScene(device, RTC_SCENE_STATIC, RTC_INTERSECT1);
-        unsigned geom_id = rtcNewTriangleMesh2(scenes[i], RTC_GEOMETRY_STATIC, meshes[i]->GetTriangleCount(), meshes[i]->GetVertexCount(), 1);
+    auto scene = rtcDeviceNewScene(device, RTC_SCENE_STATIC, RTC_INTERSECT1);
+    unsigned geom_id = rtcNewTriangleMesh2(scene, RTC_GEOMETRY_STATIC, mesh->GetTriangleCount(), mesh->GetVertexCount(), 1);
 
-        auto vertices = static_cast<Vertex*>(rtcMapBuffer(scenes[i], geom_id, RTC_VERTEX_BUFFER));
-        for (int32_t k = 0; k < meshes[i]->GetVertexCount(); k++, vertices++) {
-            vertices->x = meshes[i]->vertices[k].x;
-            vertices->y = meshes[i]->vertices[k].y;
-            vertices->z = meshes[i]->vertices[k].z;
-        }
-        rtcUnmapBuffer(scenes[i], geom_id, RTC_VERTEX_BUFFER);
-
-        auto triangles = static_cast<Triangle*>(rtcMapBuffer(scenes[i], geom_id, RTC_INDEX_BUFFER));
-        for (int k = 0; k < meshes[i]->GetTriangleCount(); k++, triangles++) {
-            triangles->v0 = meshes[i]->triangles[k].points[0].vertexIndex;
-            triangles->v1 = meshes[i]->triangles[k].points[1].vertexIndex;
-            triangles->v2 = meshes[i]->triangles[k].points[2].vertexIndex;
-        }
-        rtcUnmapBuffer(scenes[i], geom_id, RTC_INDEX_BUFFER);
-        rtcCommit(scenes[i]);
+    auto vertices = static_cast<Vertex*>(rtcMapBuffer(scene, geom_id, RTC_VERTEX_BUFFER));
+    for (int32_t k = 0; k < mesh->GetVertexCount(); k++, vertices++) {
+        vertices->x = mesh->vertices[k].x;
+        vertices->y = mesh->vertices[k].y;
+        vertices->z = mesh->vertices[k].z;
     }
+    rtcUnmapBuffer(scene, geom_id, RTC_VERTEX_BUFFER);
+
+    auto triangles = static_cast<Triangle*>(rtcMapBuffer(scene, geom_id, RTC_INDEX_BUFFER));
+    for (int k = 0; k < mesh->GetTriangleCount(); k++, triangles++) {
+        triangles->v0 = mesh->triangles[k].points[0].vertexIndex;
+        triangles->v1 = mesh->triangles[k].points[1].vertexIndex;
+        triangles->v2 = mesh->triangles[k].points[2].vertexIndex;
+    }
+    rtcUnmapBuffer(scene, geom_id, RTC_INDEX_BUFFER);
+    rtcCommit(scene);
 
     printf("shooting rays (embree)...\n");
     random_init();
-    for (size_t i = 0; i < models.size(); i++) {
-        int timeMsec = BenchmarkEmbree(scenes[i], meshes[i]->GetBounds());
-        double speed = (benchmarkRaysCount / 1000000.0) / (timeMsec / 1000.0);
-        printf("raycast performance [%-6s]: %.2f MRays/sec, (rnd = %d)\n", StripExtension(GetFileName(models[i])).c_str(), speed, random_uint32());
-    }
 
-    for (size_t i = 0; i < models.size(); i++) {
-        rtcDeleteScene(scenes[i]);
-    }
+    int timeMsec = BenchmarkEmbree(scene, mesh->GetBounds());
+    double speed = (benchmarkRaysCount / 1000000.0) / (timeMsec / 1000.0);
+    printf("raycast performance [%-6s]: %.2f MRays/sec, (rnd = %d)\n", StripExtension(GetFileName(model_path)).c_str(), speed, random_uint32());
+
+    rtcDeleteScene(scene);
     rtcDeleteDevice(device);
 }
 
